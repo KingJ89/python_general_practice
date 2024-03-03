1978c664bdc4570801dc5132b7dc112c8df8aed2
KingJ89 <janmutewera@gmail.com>
2024-03-03 18:39:18 +0200

    task idk chores

---
+++ oop_methods.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+class human:
+    def say_hi(self):
+        print('Hello, how are you?')
+
+
+        h = human()
+        p.say_hi()
+
+        #the previous two lines can also be written as:
+        #human().say_hi()
---
+++ oop_simplestclass.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+class human:
+    pass #An empty block
+
+h = human()
+print(h)
